8e7f43323135349076c45e6120844895e5947cfc
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-06 09:26:14 +0200

    Lesson 4 done

--- week_1/14.js
+++ week_1/14.js
@@ -2,7 +2,8 @@ var button_add = document.getElementById("add"),
     input = document.querySelector("input"),
     show_numbers = document.getElementById("show_numbers"),
     show_sum = document.getElementById("show_sum"),
-    button_add_up = document.getElementById("add_up_button");
+    button_add_up = document.getElementById("add_up_button"),
+    reset_button = document.getElementById("reset_button");
 
 var numbers_to_show = "",
     numbers = [];
@@ -15,9 +16,9 @@ var numbers_to_show = "",
 
         if(input.value) {
     
-            numbers.push( parseInt(input.value)  )
+            numbers.push( parseFloat(input.value)  )
 
-            numbers_to_show ? ( numbers_to_show += ", " + String(input.value) ) : (  numbers_to_show += String(input.value) );
+            numbers_to_show ? ( numbers_to_show += " + " + String(input.value) ) : (  numbers_to_show += String(input.value) );
     
             input.value = "";
             input.focus();
@@ -33,6 +34,19 @@ var numbers_to_show = "",
     button_add_up.addEventListener("click", function(){
 
         show_sum.innerText = sum(numbers);
+    })
+
+    reset_button.addEventListener("click", function(e){
+        e.preventDefault();
+        numbers = [];
+        numbers_to_show = "";
+        input.placeholder = "";
+        show_numbers.innerText = "";
+        input.value = "";
+        input.focus();
+        show_sum.innerText = "";
+
+
     })
 
 
