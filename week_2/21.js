40a1ab2022a9c5b17edd7e60f32e6e0c13cf51db
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-10 14:00:34 +0200

    Task 22 beginning

--- week_2/21.js
+++ week_2/21.js
@@ -1,10 +1,25 @@
 console.log("tydzien II");
 
-var button = document.getElementById("show_button"),
-    div = document.getElementById("hidden_div");
+var button_1 = document.getElementById("show_button_1"),
+    div_1 = document.getElementById("hidden_div_1"),
+    button_2 = document.getElementById("show_button_2"),
+    div_2 = document.getElementById("hidden_div_2");
 
-console.log(button);
+// console.log(button);
 
-button.addEventListener("click", function(){
-    div.classList.toggle("hidden");
+button_1.addEventListener("click", function(){
+
+    button_1.innerText == "Pokaż treść" ? button_1.innerText = "Ukryj treść" : button_1.innerText = "Pokaż treść";
+
+    div_1.classList.toggle("hidden");
+    div_1.classList.toggle("d-inline-block");
+}),
+
+button_2.addEventListener("click", function(){
+
+    button_2.innerText == "Pokaż treść" ? button_2.innerText = "Ukryj treść" : button_2.innerText = "Pokaż treść";
+
+    div_2.classList.toggle("invisible");
+    // div_2.classList.toggle("d-inline-block");
 })
+
