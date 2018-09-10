1544f94cc0fb8e1d0ea226b0030ebd2380461016
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-10 11:19:14 +0200

    Week II beginning

--- week_2/21.js
+++ week_2/21.js
@@ -1,1 +1,10 @@
 console.log("tydzien II");
+
+var button = document.getElementById("show_button"),
+    div = document.getElementById("hidden_div");
+
+console.log(button);
+
+button.addEventListener("click", function(){
+    div.classList.toggle("hidden");
+})
