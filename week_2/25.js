d2adb41d98625824b053ac02b4618b9113fcb4e1
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-19 08:15:25 +0200

    task 25 DONE !!!

--- week_2/25.js
+++ week_2/25.js
@@ -1,1 +1,55 @@
-console.log("nadupca 25");
+(function() {
+
+    var start_button = document.getElementById("start");
+
+    function debounce(fn, time) {
+
+        var time = time,
+            check = 0 ;
+
+        return function() {
+
+            check++;
+
+            var clear = setTimeout(function(){
+
+                if (check > 1){
+                    clearTimeout(clear);
+                    check --;
+                } else {
+                    fn();
+                    check = 0;
+                }
+
+            }, time);
+
+        };
+    }
+
+    var test = debounce( function(){
+        console.log("z debance");
+    }, 3000);
+
+
+    start_button.addEventListener("click", test);
+
+
+    var handleScroll = debounce(function() {
+        console.log("Scrollujemy!");
+    }, 200);
+
+    // w tym miejscu pod handleScroll
+    // powinna być nowa funkcja
+
+    // Przypisanie zdarzenia "scroll"
+    window.addEventListener("scroll", handleScroll, false);
+
+    // Możesz również przetestować funkcję
+    // ze zdarzeniem "resize" skalując okno
+    var handleResize = debounce(function() {
+        console.log("Zmieniamy rozmiar okna!");
+    }, 100);
+
+    window.addEventListener("resize", handleResize, false);
+
+})();
