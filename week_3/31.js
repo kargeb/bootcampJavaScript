960db21e29cc33facbe59f6aaece586c48650bb9
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-19 09:53:40 +0200

    week 3 has started

---
+++ week_3/31.js
@@ -0,0 +1,9 @@
+console.log("week 3");
+
+if( typeof String.prototype.repeat ) {
+    console.log('String posiada juz funkcje "repeat" więc robimy inną o nazwie "iterate"');
+} else {
+    console.log("String nie posiada tutaj funkcji 'repeat'");
+}
+
+console.log( typeof String.prototype.repeat  );
