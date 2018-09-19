30e44828a073cd6f9f4e90e2e46501837494f635
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-19 14:55:41 +0200

    task 32 in progress

--- week_3/31.js
+++ week_3/31.js
@@ -1,64 +1,65 @@
 
 (function(){
-
+    
+    if (typeof String.prototype.repeat) {
+        console.log('Function named \"repeat\" already exist in JavaScript, use \"iterate\" function instead.');
+    
+        String.prototype.iterate = function (times) {
+    
+            try {
+                if (typeof times === "number" && times >= 0) {
+    
+                    var text = "";
+    
+                    while (times) {
+                        text += this;
+                        times--;
+                    }
+    
+                    return text;
+    
+                } else {
+                    throw new Error("Wrong value! Only positive numbers are allowed.");
+                }
+    
+            } catch (e) {
+                console.log(e.message);
+            }
+    
+        }
+    
+    } else {
+        console.log("Function named \"repeat\" doesn't exist in JavaScript, so that you can use polyfill with the same name");
+    
+        String.prototype.repeat = function (times) {
+    
+            try {
+    
+                if (typeof times === "number" && times >= 0) {
+    
+                    var text = "";
+    
+                    while (times) {
+                        text += this;
+                        times--;
+                    }
+    
+                    return text;
+    
+                } else {
+                    throw new Error("Wrong value! Only positive numbers are allowed.");
+                }
+    
+    
+            } catch (e) {
+                console.log(e.message);
+            }
+    
+        }
+    }
+    
 })();
 
-if (typeof String.prototype.repeat) {
-    console.log('Function named \"repeat\" already exist in JavaScript, use \"iterate\" function instead.');
-
-    String.prototype.iterate = function (times) {
-
-        try {
-            if (typeof times === "number" && times >= 0) {
-
-                var text = "";
-
-                while (times) {
-                    text += this;
-                    times--;
-                }
-
-                return text;
-
-            } else {
-                throw new Error("Wrong value! Only positive numbers are allowed.");
-            }
-
-        } catch (e) {
-            console.log(e.message);
-        }
-
-    }
-
-} else {
-    console.log("Function named \"repeat\" doesn't exist in JavaScript, so that you can use polyfill with the same name");
-
-    String.prototype.repeat = function (times) {
-
-        try {
-
-            if (typeof times === "number" && times >= 0) {
-
-                var text = "";
-
-                while (times) {
-                    text += this;
-                    times--;
-                }
-
-                return text;
-
-            } else {
-                throw new Error("Wrong value! Only positive numbers are allowed.");
-            }
-
-
-        } catch (e) {
-            console.log(e.message);
-        }
-
-    }
-}
 
 
 
---
+++ week_3/32.js
@@ -0,0 +1,15 @@
+console.log("wreepepe");
+
+var elem = new Toggler("#section");
+var button = document.querySelector("#button");
+
+button.addEventListener("click", function() {
+
+    if(elem.getElem().style.display == "none") {
+        elem.show();
+    } else {
+        elem.hide();
+    }
+
+}, false);
+
