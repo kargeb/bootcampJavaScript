c854dbdb23ae85f86880e799b70a5bc535a40641
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-09-19 12:32:35 +0200

    Lesson 31 done but without frills

--- week_3/31.js
+++ week_3/31.js
@@ -1,9 +1,64 @@
-console.log("week 3");
 
-if( typeof String.prototype.repeat ) {
-    console.log('String posiada juz funkcje "repeat" więc robimy inną o nazwie "iterate"');
+(function(){
+
+})();
+
+if (typeof String.prototype.repeat) {
+    console.log('Function named \"repeat\" already exist in JavaScript, use \"iterate\" function instead.');
+
+    String.prototype.iterate = function (times) {
+
+        try {
+            if (typeof times === "number" && times >= 0) {
+
+                var text = "";
+
+                while (times) {
+                    text += this;
+                    times--;
+                }
+
+                return text;
+
+            } else {
+                throw new Error("Wrong value! Only positive numbers are allowed.");
+            }
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    }
+
 } else {
-    console.log("String nie posiada tutaj funkcji 'repeat'");
+    console.log("Function named \"repeat\" doesn't exist in JavaScript, so that you can use polyfill with the same name");
+
+    String.prototype.repeat = function (times) {
+
+        try {
+
+            if (typeof times === "number" && times >= 0) {
+
+                var text = "";
+
+                while (times) {
+                    text += this;
+                    times--;
+                }
+
+                return text;
+
+            } else {
+                throw new Error("Wrong value! Only positive numbers are allowed.");
+            }
+
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    }
 }
 
-console.log( typeof String.prototype.repeat  );
+
+
