972239a9dc8cf39975422c6c75d4d285ba1fa0f3
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-01 13:36:16 +0200

    task 33 done, without frills

--- week_3/33.js
+++ week_3/33.js
@@ -1,100 +1,66 @@
-console.log("fecz");
-
-var button = document.querySelector("button");
-var alert = document.querySelector("div[role='alert']");
-var address = "http://code.eduweb.pl/bootcamp/users/";
-console.log(button);
-console.log(alert);
-
 if( typeof fetch == "function" ){
     console.log("jest fetch wiec robimy fecz");
-
-
+    
+    
     function fecz( url, fn_success, fn_failure ){
         
         console.log(url);
-
-
-
-        var xhr = new XMLHttpRequest();
-
+        
+        
+        let mesage = "Error !";
+        
+        let xhr = new XMLHttpRequest();
+        
         xhr.open("GET", url, true);
-
-
+        
+        
         
         xhr.onreadystatechange = function(e){
-
+            
             
             
             if( this.readyState == 4 && this.status == 200 ) {
                 
-                console.log( "JEST GIT");
                 
-                console.log(this);
-
-                console.log( this.status );
-
+                fn_success(xhr.response);
                 
-                console.log( this.readyState );
-                
-                console.log(e.type);
-
-                console.log(xhr.response);
-
-                good();
-
-
                 
             } else if ( this.readyState == 4 && this.status != 200 ) {
                 
                 
-                console.log( "zjebalo sie" );
-
-                // console.log( e.type );
-                // console.log(e);
-            
-                // console.log( )
-
-                // console.log(e);
-                console.log(this);
-
                 xhr.onerror = function(e){
-                    console.log(" dupa dupa dupa");
-                    console.log(e);
-
-                    bad();
-
+                    fn_failure(mesage);
+                    
                 }
-
             }
-            
         }
-
-
+        
         xhr.send(null);
-       
     } 
     
 } else {
-    console.log("nie ma fecz nie ma nie ma nicz");
+    console.log("nie ma fetch wiec robimy fetch");
 }
 
+let button = document.querySelector("button"),
+    alert = document.querySelector("div[role='alert']"),
+    address = "http://code.eduweb.pl/bootcamp/users/";
 
 
-function good(){
-    console.log("Great");
+function good(content){
     alert.classList.add("alert-success");
     alert.innerText = "Great";
+    console.log(content);
 }
 
-function bad(){
-    console.log("Fucked up");
+function bad(message){
     alert.classList.add("alert-danger");
-    alert.innerText = "Fucked up";
+    alert.innerText = message;
+    console.log(message);
 }
 
 button.addEventListener("click", function(){
 
-    fecz(address);
+    fecz(address, good, bad);
 
 });
