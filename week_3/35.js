f2ea219bcc188adf15b7535f6dcfca2fcbf9dbb7
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-01 14:58:41 +0200

    task 35 beginning

---
+++ week_3/35.js
@@ -0,0 +1,9 @@
+console.log("35");
+
+var button_add_page = document.getElementById("button");
+
+button_add_page.addEventListener("click", function(){
+
+    window.location.href = "35.html?page=2";
+
+});
