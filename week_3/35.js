af676e20e53bac86a124c4fa0e246bdd8dca67be
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-02 12:28:27 +0200

    task 35 done but it needs lots of corrections

--- week_3/35.js
+++ week_3/35.js
@@ -1,12 +1,82 @@
 console.log("35");
 
-var button_add_page = document.getElementById("button");
+var button_add_page = document.getElementById("button_add_page"),
+    button_goToURL = document.getElementById("button_goToURL"),
+    button_check = document.getElementById("button_check"),
+    input = document.querySelector("input[type='text']"),
+    buttons_container = document.getElementById("buttons_container"),
+    result = document.querySelector("div[role='alert']");
+    
+var regex = /\/35\.html\?page=\d+$/;
 
-button_add_page.addEventListener("click", function(){
+var input_url ="";
+
+input.value = window.location.href;
+
+
+/* button_add_page.addEventListener("click", function(){
 
     window.location.href = "35.html?page=2";
 
+}); */
+
+console.log(input);
+console.log(buttons_container);
+
+
+function getPage(){
+
+    if ( regex.test( window.location.href  ) ){
+
+        result.innerText = "jest";
+
+        // var page_number = 
+
+        return /\d*$/.exec( window.location.href )[0];
+
+    } else {
+        result.innerText = "nie ma";
+    }
+}
+
+button_goToURL.addEventListener("click", function(){
+
+    // console.log( window.location.href + input_url );    
+    let temp = input_url;
+    input.value = "";
+
+    window.location.href += temp; 
+
+    // console.log( getPage() );
+
 });
 
-var regex = /\/35\.html\?page=\d+$/
+button_check.addEventListener("click", function(){
+
+    getPage();
+
+})
+
+buttons_container.addEventListener("click", function(e){
+
+    console.log(input.innerText);
+
+    if ( e.target.className !== "col" ){
+        console.log(e.target.innerText);
+
+        input.value += e.target.innerText;
+
+        input_url = input.value;
+
+    }
+
+    console.log(input.value);
+
+
+
+        // console.log("nie wypisuj nicz");
+    // console.log(e);
+
+})
+
 
