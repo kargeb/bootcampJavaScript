0aa64fedce1e282eb13bbdfce2c9d8f2b040b259
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-03 18:01:34 +0200

    task 41 finally DONE and it looks FUCKING AWESOME

--- week_4/41.js
+++ week_4/41.js
@@ -6,45 +6,63 @@ $(document).ready(function(){
     console.log($);
 
     var buttons = $(".line_break_buttons button");
-    var sample = $("div.grid.grid-12");
-
-    var boxes = $(":checkbox:checked");
-
-    console.log($(buttons));
-
-    // $(buttons[0]).css("background-color", "#dc3545");
-
-    console.log(boxes.parent());
+    // var sample = $(".nav > a[href^='http']");
+    // var sample = $(".nav a[href^='http']");
+    var sample = $(".pagination-item:not(span)");
 
     console.log(sample);
 
+    // console.log(sample.toggleClass("task41_bg_button_first") );
+
     $(buttons[0]).on("click", function(){
 
+        // $(this).toggleClass("task41_bg_button_first");
+        var divs = $("div.grid.grid-12");
+
         $(this).toggleClass("task41_bg_button_first");
+
+        $(divs).toggleClass("task41_bg_button_first");
 
     })
 
     $(buttons[1]).on("click", function(){
+        
+        var links = $(".nav a[href^='http']");
 
         $(this).toggleClass("task41_bg_button_second");
+
+        $(links).toggleClass("task41_bg_button_second");
+
 
     })
 
     $(buttons[2]).on("click", function(){
 
+        var unchecked = $("input:radio:not(:checked), input:checkbox:not(:checked)");
+
         $(this).toggleClass("task41_bg_button_third");
+
+        $(unchecked).parent().toggleClass("task41_bg_button_third");
 
     })
 
     $(buttons[3]).on("click", function(){
 
+        var paragraphs = $("div#text p:empty");
+
         $(this).toggleClass("task41_bg_button_fourth");
+
+        $(paragraphs).toggleClass("task41_bg_button_fourth");
 
     })
 
     $(buttons[4]).on("click", function(){
 
+        var paginations = $(".pagination-item:not(span)");
+
         $(this).toggleClass("task41_bg_button_fifth");
+
+        $(paginations).toggleClass("task41_bg_button_fifth");
 
     })
 
