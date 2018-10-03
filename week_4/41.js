7fbc4291805c101ef5534de53db00f5870f23756
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-03 14:21:58 +0200

    task 41 still pending

--- week_4/41.js
+++ week_4/41.js
@@ -5,27 +5,66 @@ $(document).ready(function(){
     console.log("jquery");
     console.log($);
 
+    var buttons = $(".line_break_buttons button");
     var sample = $("div.grid.grid-12");
 
     var boxes = $(":checkbox:checked");
+
+    console.log($(buttons));
+
+    // $(buttons[0]).css("background-color", "#dc3545");
 
     console.log(boxes.parent());
 
     console.log(sample);
 
+    $(buttons[0]).on("click", function(){
 
-    function show(elems){
+        $(this).toggleClass("task41_bg_button_first");
 
-        elems.each( function(){
+    })
 
-            $(this).css("background-color", "#dc3545");
+    $(buttons[1]).on("click", function(){
+
+        $(this).toggleClass("task41_bg_button_second");
+
+    })
+
+    $(buttons[2]).on("click", function(){
+
+        $(this).toggleClass("task41_bg_button_third");
+
+    })
+
+    $(buttons[3]).on("click", function(){
+
+        $(this).toggleClass("task41_bg_button_fourth");
+
+    })
+
+    $(buttons[4]).on("click", function(){
+
+        $(this).toggleClass("task41_bg_button_fifth");
+
+    })
+
+
+    // function show(elems){
+
+    //     elems.each( function(){
+
+    //         $(this).css("background-color", "#dc3545");
     
-        });
+    //     });
 
-    }
+    // }
 
-    sample.css("background-color", "#dc3545");
-    boxes.parent().css("background-color", "#dc3545");
+    // sample.css("background-color", "#dc3545");
+    // boxes.parent().css("background-color", "#dc3545");
+
+
+
+
 
     // show(sample);
 
