5146ed1850c196832d34aff7947d864006f28558
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-03 11:58:16 +0200

    task 41 on the right track

--- week_4/41.js
+++ week_4/41.js
@@ -1,1 +1,35 @@
-console.log("jquery");
+(function($){
+
+$(document).ready(function(){
+
+    console.log("jquery");
+    console.log($);
+
+    var sample = $("div.grid.grid-12");
+
+    var boxes = $(":checkbox:checked");
+
+    console.log(boxes.parent());
+
+    console.log(sample);
+
+
+    function show(elems){
+
+        elems.each( function(){
+
+            $(this).css("background-color", "#dc3545");
+    
+        });
+
+    }
+
+    sample.css("background-color", "#dc3545");
+    boxes.parent().css("background-color", "#dc3545");
+
+    // show(sample);
+
+
+});
+
+})(jQuery);
