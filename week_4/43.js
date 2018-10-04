fe69dd56b27df5b8c5e785c853d2b50cb687d522
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-04 09:05:18 +0200

    task 43 beginning

---
+++ week_4/43.js
@@ -0,0 +1,15 @@
+(function($){
+
+    $(document).ready(function(){
+    
+      var button_input = $("#button-addon2"),
+          pe = $("#poka");
+
+          console.log(button_input);
+          console.log(pe);
+    
+        
+    
+    });
+    
+})(jQuery);
