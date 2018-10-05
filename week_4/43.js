f87111860b7a9f169134124300ee80b48514e6cf
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-05 18:16:51 +0200

    task 44 beginning

--- week_4/43.js
+++ week_4/43.js
@@ -5,36 +5,16 @@
     var button_input = $("#button-addon2"),
         input = $("input")
         list = $("#task43_list"),
-        // items = $(list.children()),
-        // tab = new Array(list.children().length);
         tab = [];
-        
-        console.log(button_input);
-        console.log(list.children());
-        console.log(list.children().length);
-        console.log(tab);
-        console.log(input);
-
-        console.log(input.attr("placeholder")) ; 
-
-    
-        console.log(tab);
-
-        
 
         button_input.on("click", function(){
-
             show_the_list();
-
-            
         })
         
         input.on("keypress", function(e){
-            
             if(e.key == "Enter"){
                 show_the_list();
             }
-
         })
         
         function show_the_list(){
@@ -42,12 +22,8 @@
             if( input.val() ){
     
                 list.children().remove();
-    
-                // console.log(tab);
-    
                 input.attr("placeholder", "Wprowadź tekst" );
     
-                
                 tab.unshift( input.val() );
     
                 for(var i=0; i<tab.length; i++){
@@ -66,33 +42,11 @@
                         list.append(temp);
                     }
                 }
-    
                 input.val("");
             } else {
                 input.attr("placeholder", "WPISZ WARTOŚĆ!!!");
             }
         }
-        
     });
     
 })(jQuery);
-
-/*           
-          console.log(items);
-          items.first().css("display", "none");
-          items.last().css("display", "block")
-          
-          items.each( function(i, elem){
-              
-              console.log(" dla indexu " + i + " wartoasc w item yo " + $(elem).text());
-              
-              // $(elem).text(tab[i]);
-              $(elem).text(tab[i]);
-              
-          } )
-          
-          items.first().fadeIn("slow");
-          items.last().fadeOut("slow");
-          tab.length = tab.length-1;
-          console.log(tab); 
-          */
---
+++ week_4/44.js
@@ -0,0 +1,9 @@
+(function($){
+
+    $(document).ready(function(){
+    
+        console.log("44");
+    
+    });
+    
+})(jQuery);
