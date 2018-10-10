e35e200e3c59edf9086016cf7440ac4e655c7749
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-10 12:53:37 +0200

    task 44 done but frills are rearranged for afternoon

--- week_4/44.js
+++ week_4/44.js
@@ -19,23 +19,47 @@
                 dataJSON = data;
                 console.log(dataJSON);
 
-                for( var key in dataJSON){
-                    console.log( "key [" + key + "] = " + dataJSON[key].name);
-                }
+                // for( var key in dataJSON){
+                //     console.log( "key [" + key + "] = " + dataJSON[key].name);
+                // }
 
                 $(dataJSON).each(function(i, elem){
 
                     console.log( elem );
 
-                    ul.append("<li>" + elem.name + "</li>");
+                    ul.append("<li>" + elem.name + ", " + elem.username + ", " + elem.email + ", " + elem.phone + "</li>");
 
                 })
 
             });
-
-            
-
         })
+
+        function showTable(JSONobject){
+
+            // var trThead = 
+            console.log("globalna funkcja");
+
+            var headTable = $("<table>", {
+                "class" : "table table-hover"
+            }).append("<thead><tr>");
+
+            headTable.find("tr").append("<th>#</th>")
+                                .append("<th>Name</th>")
+                                .append("<th>Username</th>")
+                                .append("<th>Email</th>")
+                                .append("<th>Phone</th>");
+
+            var bodyTable =$("<tbody>").append("<tr>").append("<th>duap daupd</th>");                    
+
+            headTable.find("thead").after(bodyTable);
+
+            headTable.insertBefore(downloadButton);
+
+
+
+        }
+
+        showTable();
 
     });
     
