31dbd924c71183491c1c3d494e246ed30eac758a
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-10 11:33:59 +0200

    task 44 in progress

--- week_4/44.js
+++ week_4/44.js
@@ -1,9 +1,42 @@
 (function($){
 
     $(document).ready(function(){
-    
-        console.log("44");
-    
+
+        var downloadButton = $("#downloadButton"),
+            url = "http://code.eduweb.pl/bootcamp/users/",
+            dataJSON;
+
+            ul = $('<ul>');
+            ul.insertAfter(downloadButton);
+
+
+        downloadButton.on("click", function(){
+
+            console.log("works");
+
+            $.getJSON(url,function(data){
+                
+                dataJSON = data;
+                console.log(dataJSON);
+
+                for( var key in dataJSON){
+                    console.log( "key [" + key + "] = " + dataJSON[key].name);
+                }
+
+                $(dataJSON).each(function(i, elem){
+
+                    console.log( elem );
+
+                    ul.append("<li>" + elem.name + "</li>");
+
+                })
+
+            });
+
+            
+
+        })
+
     });
     
 })(jQuery);
