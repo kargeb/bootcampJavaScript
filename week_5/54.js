d0a4e91cebff7aaddabd6927582842fef49c8f5a
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-13 17:24:37 +0200

    task 54 in progress

--- week_5/54.js
+++ week_5/54.js
@@ -1,7 +1,8 @@
 console.log("lotek");
 
 const inputNewNumber = document.getElementById("inputNewNumber"),
-      buttonNewNumber = document.getElementById("buttonNewNumber"); 
+      buttonNewNumber = document.getElementById("buttonNewNumber"),
+      gotNumbers = document.getElementById("gotNumbers");
 
 
 let newNumbers = [];
@@ -12,13 +13,37 @@ buttonNewNumber.addEventListener("click", function(){
 
     // console.log( inputNewNumber.value );
 
-    inputNewNumber.value ? newNumbers.push( inputNewNumber.value ) : inputNewNumber.placeholder = "Podaj liczbę!";
+
+    inputNewNumber.value ? inserNewNumber( Number(inputNewNumber.value)) : inputNewNumber.placeholder = "Podaj liczbę!";
 
     inputNewNumber.value = "";
 
     console.log(newNumbers);
 
+    if(newNumbers.length == 6){
+        buttonNewNumber.disabled = true;
+        inputNewNumber.placeholder = "Podałeś wszystkie 6 liczb";
+    }
+
+    inputNewNumber.focus();
+
 })
+
+function inserNewNumber(number){
+
+    console.log( typeof number );
+
+    if(Number.isInteger(number) && number > 0 && number < 49 ){
+
+        newNumbers.push(number);
+        gotNumbers.innerText = newNumbers.join(", ");
+
+
+    } else {
+        inputNewNumber.placeholder = "LIczba musi być CAŁKOWITA, większa od 0 i w zakresie 1-49";
+    }
+
+}
 
 function draw(max){
     
