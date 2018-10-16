01bb917b67df41edb3baa78dcc85cbc940803dd9
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-16 13:01:24 +0200

    task55 in progress

---
+++ week_5/55.js
@@ -0,0 +1,44 @@
+console.log("55");
+
+const   downloadButton = document.getElementById("downloadButton"),
+        poka = document.getElementById("poka");
+
+const urls = [
+    "http://code.eduweb.pl/kurs-jquery/images/photo-1.jpg",
+    "http://code.eduweb.pl/kurs-jquery/images/photo-2.jpg",
+    "http://code.eduweb.pl/kurs-jquery/images/photo-3.jpg",
+    "http://code.eduweb.pl/kurs-jquery/images/photo-4.jpg"
+];
+
+downloadButton.addEventListener("click", function(){
+
+
+    getPicture(urls[0]).then(
+        picture => poka.append(picture)
+    );
+    
+    
+})        
+
+function getPicture(url){
+    
+    
+    let picture = document.createElement("img");
+    
+        picture.src = url;
+    
+        
+        let p = new Promise(function(resolve, reject){
+            
+            picture.addEventListener("load", function(){
+        
+                resolve(picture);
+                console.log("finished loading");
+            })
+            
+    
+    
+        })
+        // poka.append(picture);
+        return p;
+}
