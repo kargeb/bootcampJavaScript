db7daf8d35b2c0ded460a39441cdd5c68c208f44
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-10-27 18:00:02 +0200

    rwd beginning

---
+++ index.js
@@ -0,0 +1,15 @@
+let buttons = document.getElementById("buttons"),
+    tasks = document.getElementById("tasks");
+
+// console.log(button);
+
+buttons.addEventListener("click", function(e){
+
+
+    console.log(e.target.id);
+    // console.log(e);
+    // console.log(this);
+    // console.log(this.id);
+    // console.log(this);
+
+})
