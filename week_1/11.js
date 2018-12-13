e1e2ac39204068c6f8b9dd4ad3f02fe223b81c5f
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-12-13 22:49:57 +0100

    asd

--- week_1/11.js
+++ week_1/11.js
@@ -1,95 +1,93 @@
-var discount_output = document.getElementById("discount"),
-    products_output = document.getElementById("products"),
-    before_discount = document.getElementById("before_discount"),
-    after_discount = document.getElementById("after_discount"),
-    discount_table = document.getElementById("discount_table"),
-    button_calc = document.getElementById("calculate_button"),
-    button_reset = document.getElementById("reset_button"),
-    product_price_field = document.getElementById("product_price"),
-    amount_field = document.getElementById("amount_field"),
-    hidden_row = document.getElementById("hiddenRow"),
-    input_product_price = document.getElementById("price"),
-    input_product_amount = document.getElementById("amount");
+var input_productPrice = document.getElementById("price"),
+    input_productsAmount = document.getElementById("amount"),
+    output_currentDiscount = document.getElementById("discount"),
+    output_allOrders = document.getElementById("products"), 
+    output_orderPriceBeforeDiscount = document.getElementById("before_discount"),
+    output_orderPriceAfterDiscount = document.getElementById("after_discount"),
+    output_productPrice = document.getElementById("product_price"),
+    output_productsAmount = document.getElementById("amount_field"),
+    discountTable = document.getElementById("discount_table"),
+    button_calculateOrder = document.getElementById("calculate_button"),
+    button_resetAllOrders = document.getElementById("reset_button"),
 
-var discount_table_rows = discount_table.children[1].children;
-
-var number_of_products = 0,
+    discountTableRows = discountTable.children[1].children,
+    allOrders = 0,
     discount = 0,
     sum,
-    color_cell = null;  
+    highlightedRow = null;  
 
-input_product_price.addEventListener("keypress", function(e){
+input_productPrice.addEventListener("keypress", function(e){
     if( e.key == "Enter" ){
-        input_product_amount.focus();
+        input_productsAmount.focus();
     }
 })
 
-input_product_amount.addEventListener("keypress", function(e){
+input_productsAmount.addEventListener("keypress", function(e){
     if( e.key == "Enter" ){
-        button_calc.focus();
+        button_calculateOrder.focus();
     }
 })
 
-button_calc.addEventListener("click", function () {
+button_calculateOrder.addEventListener("click", function () {
 
-    var product_price = parseInt(input_product_price.value);
-    var amount = parseInt(input_product_amount.value);
+    var product_price = parseInt(input_productPrice.value);
+    var amount = parseInt(input_productsAmount.value);
 
     if( !product_price ) {
-        input_product_price.placeholder = "Podaj cenę!";
-        input_product_price.focus();
+        input_productPrice.placeholder = "Podaj cenę!";
+        input_productPrice.focus();
         return false;
     } else if (!amount) {
-        input_product_amount.placeholder = "Podaj cenę!";
-        input_product_amount.focus();
+        input_productsAmount.placeholder = "Podaj cenę!";
+        input_productsAmount.focus();
         return false;
     } else {
-        input_product_price.placeholder = "-";
-        input_product_amount.placeholder = "-";
+        input_productPrice.placeholder = "-";
+        input_productsAmount.placeholder = "-";
     }
 
-    number_of_products += amount;
-    discount = calculateDiscount(number_of_products);
+    allOrders += amount;
+    discount = calculateDiscount(allOrders);
 
-    products_output.innerText = number_of_products;
-    discount_output.innerText = discount + "%";
+    output_allOrders.innerText = allOrders;
+    output_currentDiscount.innerText = discount + "%";
 
-    var sum = (amount * product_price) - (amount * product_price) * discount / 100;
+    sum = (amount * product_price) - (amount * product_price) * discount / 100;
 
-    before_discount.innerHTML =  amount * product_price + " zł";
-    after_discount.innerText = sum + " zł";
-    product_price_field.innerText = product_price;
-    amount_field.innerText = amount;
+    output_orderPriceBeforeDiscount.innerHTML =  amount * product_price + " zł";
+    output_orderPriceAfterDiscount.innerText = sum + " zł";
+    output_productPrice.innerText = product_price;
+    output_productsAmount.innerText = amount;
 
-    button_calc.innerText = "Oblicz kolejne zakupy";
+    button_calculateOrder.innerText = "Oblicz kolejne zakupy";
 
     console.log("Podstawowa cena produktów to " + amount * product_price + " zł, po obniżce to " + sum + " zł");
 
-    input_product_price.value = "";
-    input_product_amount.value = "";
-    input_product_price.focus();
+    input_productPrice.value = "";
+    input_productsAmount.value = "";
+    input_productPrice.focus();
 })
 
-button_reset.addEventListener("click", function () {
+button_resetAllOrders.addEventListener("click", function () {
 
-        number_of_products = 0;
+        allOrders = 0;
         discount = 0;
-        sum = 0;
-        input_product_price.focus();
-        products_output.innerText = " - ";
-        discount_output.innerText = " - ";
-        before_discount.innerText = " - ";
-        after_discount.innerText = " - ";
-        product_price_field.innerText = "-";
-        amount_field.innerText = "-";
-        button_calc.innerText = "Oblicz pierwsze zakupy";
-        discount_table_rows[color_cell].classList.remove("table-primary");
-        color_cell = null;
+        // sum = 0;
+        input_productPrice.focus();
+        output_allOrders.innerText = " - ";
+        output_currentDiscount.innerText = " - ";
+        output_orderPriceBeforeDiscount.innerText = " - ";
+        output_orderPriceAfterDiscount.innerText = " - ";
+        output_productPrice.innerText = "-";
+        output_productsAmount.innerText = "-";
+        button_calculateOrder.innerText = "Oblicz pierwsze zakupy";
+        discountTableRows[highlightedRow].classList.remove("table-primary");
+        highlightedRow = null;
 })
 
 function calculateDiscount(amount) {
 
-    discount_table_rows[color_cell] ? discount_table_rows[color_cell].classList.remove("table-primary") : null;
+    discountTableRows[highlightedRow] ? discountTableRows[highlightedRow].classList.remove("table-primary") : null;
 
     var dis = null;
 
@@ -97,19 +95,19 @@ function calculateDiscount(amount) {
         dis = 0;
     } else if (amount >= 5 && amount < 21) {        // cos cos
         dis = 5;
-        color_cell = 0;
+        highlightedRow = 0;
     } else if (amount >= 21 && amount < 51) {       // cosa cosa cos
         dis = 10;                                   //djastd75tuyastd785678 asd
-        color_cell = 1;
+        highlightedRow = 1;
     } else if (amount >= 51 && amount < 101) {
         dis = 15;
-        color_cell = 2;
+        highlightedRow = 2;
     } else if (amount >= 101) {
         dis = 20;
-        color_cell = 3;
+        highlightedRow = 3;
     }
 
-    discount_table_rows[color_cell] ? discount_table_rows[color_cell].classList.add("table-primary") : null;
+    discountTableRows[highlightedRow] ? discountTableRows[highlightedRow].classList.add("table-primary") : null;
 
     return dis;
 }
