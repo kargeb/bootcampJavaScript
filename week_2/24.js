ed314702cab2e286c7ad4190e5bcc562d830cd6a
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-12-19 11:45:58 +0100

    24

--- week_2/24.js
+++ week_2/24.js
@@ -4,22 +4,22 @@ function EventEmitter() {
 
 }
 
-EventEmitter.prototype.on = function(type, fn) {
+EventEmitter.prototype.on = function (type, fn) {
 
-    if(!type || !fn) return;
+    if (!type || !fn) return;
 
     this.events[type] = this.events[type] || [];
     this.events[type].push(fn);
 
 }
 
-EventEmitter.prototype.emit = function(type, data) {
+EventEmitter.prototype.emit = function (type, data) {
 
     var fns = this.events[type];
 
-    if(!fns || !fns.length) return;
+    if (!fns || !fns.length) return;
 
-    for(var i = 0; i < fns.length; i++) {
+    for (var i = 0; i < fns.length; i++) {
         fns[i](data);
     }
 
@@ -36,7 +36,7 @@ function Database(url) {
 
 }
 
-Database.prototype.connect = function() {
+Database.prototype.connect = function () {
 
     // fikcyjne podłączenie do bazy
 
@@ -44,7 +44,7 @@ Database.prototype.connect = function() {
 
 }
 
-Database.prototype.disconnect = function() {
+Database.prototype.disconnect = function () {
 
     // fikcyjne zakończenie połączenia z bazą
 
@@ -52,53 +52,54 @@ Database.prototype.disconnect = function() {
 
 }
 
-
 var content = document.getElementById("content"),
-    button_run = document.getElementById("button_run");
+    button_run = document.getElementById("button_run"); 
 
-button_run.addEventListener("click", function(){
+button_run.addEventListener("click", function () {
 
-            // Użycie EventEmittera
-        var ev = new EventEmitter();
+    content.classList.remove("hidden");
 
-        ev.on("hello", function(message) {
-            console.log("Witaj " + message + "!");
-            content.innerHTML += "Witaj "  + message + "!";
-        });
+    // Użycie EventEmittera
+    var ev = new EventEmitter();
 
-        ev.on("hello", function(message) {
-            console.log("Siema " + message + ".");
-            content.innerHTML += "<br> Siema "  + message + ".";
-        });
+    ev.on("hello", function (message) {
+        console.log("Witaj " + message + "!");
+        content.innerHTML += "Witaj " + message + "!";
+    });
 
-        ev.on("goodbye", function() {
-            console.log("Do widzenia!");
-            content.innerHTML += "<br> Do widzenia! <br>";
-        });
+    ev.on("hello", function (message) {
+        console.log("Siema " + message + ".");
+        content.innerHTML += "<br> Siema " + message + ".";
+    });
 
-        ev.emit("hello", "Marek");
-        ev.emit("goodbye");
-        ev.emit("custom"); // nic się nie wydarzy
+    ev.on("goodbye", function () {
+        console.log("Do widzenia!");
+        content.innerHTML += "<br> Do widzenia! <br>";
+    });
 
-        // DO ZROBIENIA!
-        // Docelowe użycie klasy Database
-        var db = new Database("db://localhost:3000"); // fikcyjny adres
+    ev.emit("hello", "Marek");
+    ev.emit("goodbye");
+    ev.emit("custom"); // nic się nie wydarzy
 
-        db.on("connect", function(url) {
-            console.log("Połączenie z bazą pod adresem " + url + " zostało ustanowione.");
-            content.innerHTML += "<br> Połączenie z bazą pod adresem " + url + " zostało ustanowione.";
-        });
+    // DO ZROBIENIA!
+    // Docelowe użycie klasy Database
+    var db = new Database("db://localhost:3000"); // fikcyjny adres
 
-        db.on("disconnect", function(url) {
-            console.log("Połączenie z bazą pod adresem " + url + " zostało zakończone.");
-            content.innerHTML += "<br> Połączenie z bazą pod adresem " + url + " zostało zakończone.";
-        });
+    db.on("connect", function (url) {
+        console.log("Połączenie z bazą pod adresem " + url + " zostało ustanowione.");
+        content.innerHTML += "<br> Połączenie z bazą pod adresem " + url + " zostało ustanowione.";
+    });
 
-        db.connect();
+    db.on("disconnect", function (url) {
+        console.log("Połączenie z bazą pod adresem " + url + " zostało zakończone.");
+        content.innerHTML += "<br> Połączenie z bazą pod adresem " + url + " zostało zakończone.";
+    });
 
-        // po 5 sekundach rozłączamy się
-        setTimeout(function() {
-            db.disconnect();
-        }, 5000);
+    db.connect();
+
+    // po 5 sekundach rozłączamy się
+    setTimeout(function () {
+        db.disconnect();
+    }, 5000);
 
 })  
