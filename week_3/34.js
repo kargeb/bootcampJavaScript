e15303edfc1845c1eb878b41d8840b34d38e9a22
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-12-30 13:47:11 +0100

    34

--- week_3/34.js
+++ week_3/34.js
@@ -1,72 +1,107 @@
-console.log("task 34");
+if (typeof fetch == "function") {
 
-if( typeof fetch == "function" ){
-    console.log("jest fetch wiec robimy fecz");
-    
-    
-    function getJSON( url, fn_success, fn_failure ){
-        
-        console.log(url);
-        
-        
-        let mesage = "Error !";
-        
+    function fecz(url, fn_success, fn_failure) {
+
+        let message = "Wystąpił błąd!";
         let xhr = new XMLHttpRequest();
-        
+
         xhr.open("GET", url, true);
-        
         xhr.setRequestHeader("Accept", "application/json");
-        
-        
-        xhr.onreadystatechange = function(e){
-            
-            
-            
-            if( this.readyState == 4 && this.status == 200 ) {
-                
+
+        connectionCheckerList.children[0].classList.remove("hidden");
+        connectionCheckerList.children[0].innerText = "Połączenie ustanownione";
+
+        xhr.onreadystatechange = function (e) {
+
+            if (this.readyState == 4 && this.status == 200) {
+                connectionCheckerList.children[1].classList.remove("hidden");
+                connectionCheckerList.children[1].innerText = "Status połączenia: " + this.status;
+
                 let data = JSON.parse(xhr.response);
-
-
                 
                 fn_success(data);
-                
-                
-            } else if ( this.readyState == 4 && this.status != 200 ) {
-                
-                
-                xhr.onerror = function(e){
-                    fn_failure(mesage);
-                    
+
+            } else if (this.readyState == 4 && this.status != 200) {
+                xhr.onerror = function (e) {
+                    connectionCheckerList.children[1].classList.remove("hidden");
+                    connectionCheckerList.children[1].innerText = "Status połączenia: " + this.status;
+                    fn_failure(message);
                 }
             }
         }
-        
         xhr.send(null);
-    } 
-    
-} else {
-    console.log("nie ma fetch wiec robimy fetch");
+    }
 }
 
-let button = document.querySelector("button"),
-    alert = document.querySelector("div[role='alert']"),
-    address = "http://code.eduweb.pl/bootcamp/users/";
+let addressBad = "http://code.eduweb.pl/bootcamp/usersssssssssss/",
+    buttonOk = document.getElementById("buttonOk"),
+    buttonBad = document.getElementById("buttonBad"),
+    showContent = document.getElementById("showContent"),
+    connectionChecker = document.getElementById("connectionChecker"),
+    connectionCheckerList = document.querySelector("#connectionChecker ul"),
+    labelError = document.getElementById("labelError"),
+    labelSuccess = document.getElementById("labelSuccess");
 
 
-function good(data){
-    alert.classList.add("alert-success");
-    alert.innerText = "Great";
-    console.log(data);
+buttonOk.addEventListener("click", function () {
+
+    resetConnectionChecker();
+    connectionChecker.classList.remove("hidden");
+
+    fecz("http://code.eduweb.pl/bootcamp/users/",
+        function (data) {
+            connectionChecker.classList.add("alert-success");
+            labelSuccess.classList.remove("hidden");
+            showContent.innerText = writeContent(data);
+            console.log(data);
+        }, function (err) {
+            connectionChecker.classList.add("alert-danger");
+            labelError.classList.remove("hidden");
+            showContent.innerHTML = err;
+            console.log(err);
+        });
+});
+
+function writeContent(obj){
+    let content = "";
+
+    for (var key in obj) {
+
+        for (var key2 in obj[key]){
+
+            content += obj[key][key2] + " | ";
+        }
+
+        content += "\n";
+
+    }
+
+    return content;
+};
+
+buttonBad.addEventListener("click", function () {
+    resetConnectionChecker();
+    connectionChecker.classList.remove("hidden");
+    fecz(addressBad, fn_success, fn_error);
+});
+
+function fn_success(content) {
+    connectionChecker.classList.add("alert-success");
+    labelSuccess.classList.remove("hidden");
+    showContent.innerHTML = content;
+    console.log(content);
 }
 
-function bad(message){
-    alert.classList.add("alert-danger");
-    alert.innerText = message;
+function fn_error(message) {
+    connectionChecker.classList.add("alert-danger");
+    labelError.classList.remove("hidden");
+    showContent.innerHTML = message;
     console.log(message);
 }
 
-button.addEventListener("click", function(){
-
-    getJSON(address, good, bad);
-
-});
+function resetConnectionChecker() {
+    labelError.classList.add("hidden");
+    labelSuccess.classList.add("hidden");
+    connectionChecker.classList.remove("alert-success");
+    connectionChecker.classList.remove("alert-danger");
+}
