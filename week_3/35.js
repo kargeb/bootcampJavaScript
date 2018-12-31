61de49d114bbaaf1e87fb2bc400d9475f5b6cecd
Karol Gebarowski <karol.gebarowski@gmail.com>
2018-12-31 12:11:39 +0100

    35 beginning

--- week_3/35.js
+++ week_3/35.js
@@ -4,8 +4,6 @@ var button_add_page = document.getElementById("button_add_page"),
     button_goToURL = document.getElementById("button_goToURL"),
     button_check = document.getElementById("button_check"),
     input = document.querySelector("input[type='text']"),
-    // buttons_container = document.getElementById("buttons_container"),
-    // button_clear = document.getElementById("button_clear"),
     result = document.querySelector("h1[class='display-4']");
     
 var regex = /\/35\.html\?page=\d+$/;
@@ -13,10 +11,6 @@ var regex = /\/35\.html\?page=\d+$/;
 var input_url ="";
 
 console.log(result);
-
-// result. = "chuj";
-
-// console.log(resul)
 
 
 function getPage(){
@@ -57,40 +51,6 @@ button_goToURL.addEventListener("click", function(){
 
 button_check.addEventListener("click", function(){
 
-
-
     getPage();
 
 })
-
-// button_clear.addEventListener("click", function(){
-
-//     input.value = "?";
-//     window.location.href = "35.html";
-    
-
-// })
-
-// buttons_container.addEventListener("click", function(e){
-
-//     console.log(input.innerText);
-
-//     if ( e.target.className !== "col" ){
-//         console.log(e.target.innerText);
-
-//         input.value += e.target.innerText;
-
-//         input_url = input.value;
-
-//     }
-
-//     console.log(input.value);
-
-
-
-//         // console.log("nie wypisuj nicz");
-//     // console.log(e);
-
-// })
-
-
