a01ac0f72972adc74697b18fa2d84a2c8222227a
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-01-03 06:16:59 +0100

    tsk35

--- week_3/35.js
+++ week_3/35.js
@@ -3,14 +3,16 @@ console.log("35");
 var button_add_page = document.getElementById("button_add_page"),
     button_goToURL = document.getElementById("button_goToURL"),
     button_check = document.getElementById("button_check"),
-    input = document.querySelector("input[type='text']"),
+    input = document.getElementById("basic-url"),
     result = document.querySelector("h1[class='display-4']");
     
 var regex = /\/35\.html\?page=\d+$/;
 
-var input_url ="";
+var input_url ="?page=2";
 
 console.log(result);
+
+console.log(input);
 
 
 function getPage(){
@@ -37,13 +39,16 @@ function getPage(){
 
 button_goToURL.addEventListener("click", function(){
 
-    // console.log( window.location.href + input_url );    
-    let temp = input_url;
+    // console.log( window.location.href + input_url );   
+
+    let temp = input.value;
+
+    console.log(temp);
     // input.value = "";
 
     window.location.href += temp; 
 
-    getPage();
+    // getPage();
 
     // console.log( getPage() );
 
@@ -54,3 +59,9 @@ button_check.addEventListener("click", function(){
     getPage();
 
 })
+
+// function goToAddress(url){
+//     let xhr = new XMLHttpRequest();
+
+//     xhr.open("GET",url,false);
+// }
