3d1b76a4dcd4bd9f6de8a820b8e3f602d2d658fb
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-01-04 13:26:23 +0100

    35 done

--- week_3/35.js
+++ week_3/35.js
@@ -1,62 +1,27 @@
-console.log("35");
-
-var  button_goToURL = document.getElementById("button_goToURL"),
+var button_goToURL = document.getElementById("button_goToURL"),
     input_url = document.getElementById("input_url"),
     summary = document.getElementById("summary"),
     outputGetPage = document.getElementById("output_GetPage");
-    
+
 var regex = /\/35\.html\?page=\d+$/;
 
-// var input_url ="?page=2";
+function getPage() {
 
-console.log(outputGetPage);
-
-console.log(window.location.href);
-console.log(window.location.pathname);
-
-console.log(input_url);
-console.log(input_url.value);
-
-getPage();
-
-showSummary();
-
-function getPage(){
-
-    console.log("jestem");
-
-    console.log( window.location.href);
-
-    if ( regex.test( window.location.href  ) ){
-
-
-        console.log("jest teb zndhd");
-        outputGetPage.innerText = /\d*$/.exec( window.location.href )[0];
-
-
-         return  Number( outputGetPage.innerText );
+    if (regex.test(window.location.href)) {
+        return Number(/\d*$/.exec(window.location.href)[0]);
 
     } else {
-        console.log("nie ma");
-        outputGetPage.innerText = "null";
         return null;
     }
 }
 
-button_goToURL.addEventListener("click", function(){
-    
-    let temp = input_url.value;
+outputGetPage.innerText = getPage();
+showSummary();
 
-    console.log("jestem w button");
-    // console.log(input_url);
-    console.log(temp);
-
-    window.location.href = window.location.pathname + temp;
-
+button_goToURL.addEventListener("click", function () {
+    window.location.href = window.location.pathname + input_url.value;
 });
 
-function showSummary(){
-
+function showSummary() {
     summary.innerText = "adress " + window.location.href + " \n cos tam cos tam ze wyrazeniem: \n /\\/35\\.html\\?page=\\d+$/";
-
 }
