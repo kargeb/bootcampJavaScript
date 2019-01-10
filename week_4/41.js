70b8b2302b28896d5de872aea39414450096bd95
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-01-10 15:12:20 +0100

    41

--- week_4/41.js
+++ week_4/41.js
@@ -1,60 +1,47 @@
-(function($){
+(function ($) {
+    $(document).ready(function () {
 
-$(document).ready(function(){
+        var buttons = $(".selector_buttons button");
 
+        $(buttons[0]).on("click", function () {
+            var divs = $("div.grid.grid-12");
 
-    var buttons = $(".selector_buttons button");
+            $(this).toggleClass("task41_bg_button_first");
+            $(divs).toggleClass("task41_bg_button_first");
 
-    $(buttons[0]).on("click", function(){
-        var divs = $("div.grid.grid-12");
+        })
 
-        $(this).toggleClass("task41_bg_button_first");
+        $(buttons[1]).on("click", function () {
+            var links = $(".nav a[href^='http']");
 
-        $(divs).toggleClass("task41_bg_button_first");
+            $(this).toggleClass("task41_bg_button_third");
+            $(links).toggleClass("task41_bg_button_third");
+        })
 
-    })
+        $(buttons[2]).on("click", function () {
+            var unchecked = $("input:radio:not(:checked), input:checkbox:not(:checked)");
 
-    $(buttons[1]).on("click", function(){
-        
-        var links = $(".nav a[href^='http']");
+            $(this).toggleClass("task41_bg_button_second");
+            if ($(this).hasClass("task41_bg_button_second")) {
+                $("input:radio, input:checkbox").prop('disabled', true);
+            } else {
+                $("input:radio, input:checkbox").prop('disabled', false);
+            }
+            $(unchecked).parent().toggleClass("task41_bg_button_second");
+        })
 
-        $(this).toggleClass("task41_bg_button_second");
+        $(buttons[3]).on("click", function () {
+            var paragraphs = $("div#text p:empty").first();
 
-        $(links).toggleClass("task41_bg_button_second");
+            $(this).toggleClass("task41_bg_button_fourth");
+            $(paragraphs).toggleClass("task41_bg_button_fourth");
+        })
 
+        $(buttons[4]).on("click", function () {
+            var paginations = $(".pagination-item:not(span)");
 
-    })
-
-    $(buttons[2]).on("click", function(){
-
-        var unchecked = $("input:radio:not(:checked), input:checkbox:not(:checked)");
-
-        $(this).toggleClass("task41_bg_button_third");
-
-        $(unchecked).parent().toggleClass("task41_bg_button_third");
-
-    })
-
-    $(buttons[3]).on("click", function(){
-
-        var paragraphs = $("div#text p:empty");
-
-        $(this).toggleClass("task41_bg_button_fourth");
-
-        $(paragraphs).toggleClass("task41_bg_button_fourth");
-
-    })
-
-    $(buttons[4]).on("click", function(){
-
-        var paginations = $(".pagination-item:not(span)");
-
-        $(this).toggleClass("task41_bg_button_fifth");
-
-        $(paginations).toggleClass("task41_bg_button_fifth");
-
-    })
-
-});
-
+            $(this).toggleClass("task41_bg_button_fifth");
+            $(paginations).toggleClass("task41_bg_button_fifth");
+        })
+    });
 })(jQuery);
