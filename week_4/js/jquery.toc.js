b1e82e0d7b8305fb272f2adaa312ee0d719542f8
Karol Gebarowski <karol.gebarowski@gmail.com>
2019-01-12 19:04:24 +0100

    week 5

--- week_4/js/jquery.toc.js
+++ week_4/js/jquery.toc.js
@@ -1,31 +1,28 @@
-// Tutaj stwórz kod całego pluginu
+(function ($, window, document, undefined) {
 
-(function($, window, document, undefined){
-
-    $.fn.toc = function(){
+    $.fn.toc = function () {
 
         var sections = $("section");
 
         var containerToc = $("<div>", {
-            "class" : "toc"
+            "class": "toc"
         }).append("<h2>Spis treści</h2>")
             .append("<ul>");
 
         sections.first().before(containerToc);
 
-        return this.each(function(i){
+        return this.each(function (i) {
 
             $(this).attr("id", i);
 
             var li = $("<li>");
             var a = $("<a>", {
-                href : "#" + i,
-                text : (i+1) + ". " + $(this).find("h2").text()
+                href: "#" + i,
+                text: (i + 1) + ". " + $(this).find("h2").text()
             });
 
             li.append(a);
             containerToc.find("ul").append(li);
-            })
-        };
-
+        })
+    };
 })(jQuery, window, document);
